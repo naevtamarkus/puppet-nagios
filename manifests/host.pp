60a68fe8952088ad56b9cb1dbcb372653c2ffff0
Pablo Fernandez <pablo.fernandez@cscs.ch>
2013-12-03 17:58:02 +0100

    Revert "Fixed hostgroups"
    This reverts commit 4a2310f505d8a231bd75a46876c3a740816fa593.

--- manifests/host.pp
+++ manifests/host.pp
@@ -56,11 +56,10 @@ define nagios::host (
         tag                 => regsubst($server,'^(.+)$','nagios-\1'),
     }
 
-    # Create also the hostgroups (with no description)
-    $hg_array = split ($hostgroups, ',')
+    # Create also the hostgroups
+    hg_array = split ($hostgroups, ',')
     @@nagios_hostgroup {$hg_array:
-        alias => '',
-        tag   => regsubst($server,'^(.+)$','nagios-\1'),
+        alias => $name,
     }
 }
 
